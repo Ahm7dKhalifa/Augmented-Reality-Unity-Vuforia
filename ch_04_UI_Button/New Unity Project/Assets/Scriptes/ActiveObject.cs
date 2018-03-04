d99846eb4bd84ffe83cb03bb57226940892de7be
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-03-04 18:28:39 +0200

    add ch_04_UI_Button

---
+++ ch_04_UI_Button/New Unity Project/Assets/Scriptes/ActiveObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveObject : MonoBehaviour {
+
+    public GameObject cube;
+    
+    public void on()
+    {
+        cube.SetActive(true);
+    }
+
+    public void off()
+    {
+      cube.SetActive(false);
+    }
+
+}
