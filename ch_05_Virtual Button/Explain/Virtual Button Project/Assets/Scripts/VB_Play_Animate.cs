67599d67f25c781036813e8402f84c90ac7a85a0
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-03-18 21:00:24 +0200

    add ch_05_Virtual_Button

---
+++ ch_05_Virtual Button/Explain/Virtual Button Project/Assets/Scripts/VB_Play_Animate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VB_Play_Animate : MonoBehaviour,IVirtualButtonEventHandler {
+
+    public GameObject vbBtnObj;
+    public Animator cubeAni;
+
+    // Use this for initialization
+    void Start()
+    {
+        //vbBtnObj = GameObject.Find("VirtualButtonPlay");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        cubeAni.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("CubeAnimation");
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("none");
+        Debug.Log("Button released");
+    }
+}
